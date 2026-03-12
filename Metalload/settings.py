ac58f48d54e93213ffc7e33093b117e2e282ffb9
Cristian6600 <cristian6600@hotmail.es>
2026-03-12 10:26:12 -0500

    mejoras-Metalload

--- Metalload/settings.py
+++ Metalload/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'Metalload.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'Metalload/templates'],  # 🔥 Agregar directorio de templates
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
